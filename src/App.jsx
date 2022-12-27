9e05b58bdc42350e85e458e803dbbfd0702142e0
tgcidpl <tgcidpl@gmail.com>
2022-12-27 18:44:07 +0100

    search WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
+import Form from "./components/Form";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="text-center p-8 bg-slate-800 text-red-700 text-4xl">
-      <h1>YOU DIED</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {count} times
-        </button>
+    <div>
+      <Form />
+      <div className="text-center p-8 bg-slate-800 text-red-700 text-4xl">
+        <h1>YOU DIED</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            {count} times
+          </button>
+        </div>
       </div>
     </div>
   );
